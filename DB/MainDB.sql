100ab6fe4bc400412220626f7c70fc52638c27c0
=Oier Marañón <o.maranonocejo@ikasle.eus>
2026-01-13 11:38:35 +0100

    Aldaketak datubasean denboraldi bakoitza bakarrik 6 talde eduki ahal izateko, taldeak igo edo jaitsi egingo dute. Ere bai inserts aldatu egin ditut aldaketetara egokitzeko.

--- DB/MainDB.sql
+++ DB/MainDB.sql
@@ -1,78 +1,84 @@
-DROP DATABASE IF EXISTS FNFS;
-CREATE DATABASE FNFS;
-USE FNFS;
+DROP DATABASE IF EXISTS fnfs;
+CREATE DATABASE fnfs;
+USE fnfs;
 
 CREATE TABLE DENBORALDI (
-    denboraldi_id INT(4) PRIMARY KEY AUTO_INCREMENT,
-    urtea INT(4) NOT NULL UNIQUE
-);
-
-CREATE TABLE JARDUNALDI (
-    jardunaldi_id INT(4) PRIMARY KEY AUTO_INCREMENT,
-    jardunaldi_zbk INT(2) NOT NULL,
-    denboraldi_id INT(4),
-    UNIQUE (denboraldi_id, jardunaldi_zbk),
-    FOREIGN KEY (denboraldi_id) REFERENCES DENBORALDI(denboraldi_id) ON DELETE RESTRICT
-);
+    denboraldi_id INT(4) AUTO_INCREMENT PRIMARY KEY,
+    urtea YEAR NOT NULL UNIQUE
+) 
 
 CREATE TABLE TALDE (
-    talde_id INT(4) PRIMARY KEY AUTO_INCREMENT,
-    izena VARCHAR(50) NOT NULL UNIQUE,
-    sorrera_data DATE NOT NULL DEFAULT '1901-01-01',
-    presidentea VARCHAR(50)
-);
+    talde_id INT(4) AUTO_INCREMENT PRIMARY KEY,
+    izena VARCHAR(100) NOT NULL,
+    herria VARCHAR(100),
+    presidentea VARCHAR(100)
+) 
+
+CREATE TABLE TALDE_DENBORALDIA (
+    denboraldi_id INT(4),
+    talde_id INT(4),
+    PRIMARY KEY (denboraldi_id, talde_id),
+    FOREIGN KEY (denboraldi_id) REFERENCES DENBORALDI(denboraldi_id) ON DELETE CASCADE,
+    FOREIGN KEY (talde_id) REFERENCES TALDE(talde_id) ON DELETE CASCADE
+) 
+
+CREATE TABLE JOKALARI (
+    jokalari_id INT(4) AUTO_INCREMENT PRIMARY KEY,
+    izena VARCHAR(50) NOT NULL,
+    abizenak VARCHAR(100) NOT NULL,
+    jaiotze_urtea DATE
+) 
+
+CREATE TABLE JOKALARI_TALDEA (
+    jokalari_id INT(4),
+    talde_id INT(4),
+    denboraldi_id INT(4),
+    dorsala INT(2),
+    sarrera_data DATE,
+    irteera_data DATE,
+    PRIMARY KEY (jokalari_id, talde_id, denboraldi_id),
+    FOREIGN KEY (jokalari_id) REFERENCES JOKALARI(jokalari_id),
+    FOREIGN KEY (talde_id) REFERENCES TALDE(talde_id),
+    FOREIGN KEY (denboraldi_id) REFERENCES DENBORALDI(denboraldi_id)
+) 
+
+CREATE TABLE JARDUNALDI (
+    jardunaldi_id INT(4) AUTO_INCREMENT PRIMARY KEY,
+    denboraldi_id INT(4),
+    jardunaldi_zbk INT(2),
+    FOREIGN KEY (denboraldi_id) REFERENCES DENBORALDI(denboraldi_id)
+) 
 
 CREATE TABLE PARTIDU (
-    partidu_id INT(4) PRIMARY KEY AUTO_INCREMENT,
+    partidu_id INT(4) AUTO_INCREMENT PRIMARY KEY,
     jardunaldia INT(4),
     talde_lokala INT(4),
     kanpoko_talde INT(4),
-    golak_etxeko INT DEFAULT 0,
-    golak_kanpoko INT DEFAULT 0,
-    FOREIGN KEY (jardunaldia) REFERENCES JARDUNALDI(jardunaldi_id) ON DELETE RESTRICT,
-    FOREIGN KEY (talde_lokala) REFERENCES TALDE(talde_id) ON DELETE RESTRICT,
-    FOREIGN KEY (kanpoko_talde) REFERENCES TALDE(talde_id) ON DELETE RESTRICT,
-    CHECK (talde_lokala <> kanpoko_talde)
-);
+    golak_etxeko INT(2) DEFAULT 0,
+    golak_kanpoko INT(2) DEFAULT 0,
+    eguna DATE,
+    FOREIGN KEY (jardunaldia) REFERENCES JARDUNALDI(jardunaldi_id),
+    FOREIGN KEY (talde_lokala) REFERENCES TALDE(talde_id),
+    FOREIGN KEY (kanpoko_talde) REFERENCES TALDE(talde_id)
+) 
 
-CREATE TABLE JOKALARI (
-    jokalari_id INT(4) PRIMARY KEY  AUTO_INCREMENT,
-    izena VARCHAR(50) NOT NULL,
-    abizenak VARCHAR(100) NOT NULL,
-    jaiotze_urtea DATE NOT NULL DEFAULT '1901-01-01'
-);
-
-CREATE TABLE JOKALARI_TALDEA (
-    jokalari_id INT(4) NOT NULL,
-    talde_id INT(4) NOT NULL,
-    denboraldi_id INT(4) NOT NULL,
-    sarrera_data DATE NOT NULL,
-    irteera_data DATE,
-    dorsala INT(2) NOT NULL,
-    PRIMARY KEY (jokalari_id, talde_id, denboraldi_id),
-    FOREIGN KEY (jokalari_id) REFERENCES JOKALARI(jokalari_id) ON DELETE RESTRICT,
-    FOREIGN KEY (talde_id) REFERENCES TALDE(talde_id) ON DELETE RESTRICT,
-    FOREIGN KEY (denboraldi_id) REFERENCES DENBORALDI(denboraldi_id) ON DELETE RESTRICT
-);
 CREATE TABLE ERABILTZAILEA (
     erabiltzaile_id INT(4) PRIMARY KEY AUTO_INCREMENT,
     erabiltzaile VARCHAR(20) NOT NULL UNIQUE,
     pasahitza VARCHAR(255) NOT NULL,
-    email varchar(50) NOT NULL,
-    izena varchar (50) NOT NULL,
-    abizenak varchar (100) NOT NULL
-);
+    email VARCHAR(50) NOT NULL,
+    izena VARCHAR(50) NOT NULL,
+    abizenak VARCHAR(100) NOT NULL
+) 
+
 CREATE TABLE ADMINISTRARIA_ERAB (
     erabiltzaile_id INT(4) PRIMARY KEY,
     maila VARCHAR(50),
-    FOREIGN KEY (erabiltzaile_id) REFERENCES ERABILTZAILEA (erabiltzaile_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
+    FOREIGN KEY (erabiltzaile_id) REFERENCES ERABILTZAILEA(erabiltzaile_id) ON DELETE CASCADE
+) 
+
 CREATE TABLE ARBITRO_ERAB (
     erabiltzaile_id INT(4) PRIMARY KEY,
     lizentzia_zbk VARCHAR(12) NOT NULL UNIQUE,
-    FOREIGN KEY (erabiltzaile_id) REFERENCES ERABILTZAILEA (erabiltzaile_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
-CREATE TABLE PRESIDENTE_ERAB (
-    erabiltzaile_id INT(4) PRIMARY KEY,
-    FOREIGN KEY (erabiltzaile_id) REFERENCES ERABILTZAILEA (erabiltzaile_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
+    FOREIGN KEY (erabiltzaile_id) REFERENCES ERABILTZAILEA(erabiltzaile_id) ON DELETE CASCADE
+) 
